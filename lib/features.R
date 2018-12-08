9817e619cb9d63996615007194833f9d86c4759f
JudyCom <40449861+JudyCom@users.noreply.github.com>
2018-12-08 00:26:45 -0500

    Add files via upload

--- lib/features.R
+++ lib/features.R
@@ -156,10 +156,10 @@ extract_feature <- function(tesseract_list, truth_set, grams, freq_bigrams){
     ni <- length(tesseract_list[[i]])
     low_tesseract_vec <- tolower(tesseract_list[[i]])
     feature_mat <- matrix(NA, ncol = 24, nrow = ni)
-    # return(matrix(unlist(lapply(tesseract_list[[i]], cur_token_features, unlist(tesseract_list), truth_set, freq_bigrams)), byrow = TRUE, ncol = 23))
+    
     feature_mat[, -24] <- matrix(unlist(lapply(tesseract_list[[i]], cur_token_features, unlist(tesseract_list), truth_set, freq_bigrams)), byrow = TRUE, ncol = 23)
     for(j in 1:ni){
-        #f4 sliding window, 3-gram freq
+        #f24 sliding window, 3-gram freq
         f24 <- 0
         slide_window <- rep(list(NULL), 3)
         if(j >= 3){slide_window[[1]] <- paste(low_tesseract_vec[(j-2):j], collapse = " ")}
@@ -175,140 +175,6 @@ extract_feature <- function(tesseract_list, truth_set, grams, freq_bigrams){
   return(features)
 }
   
-  
-  
-#   feature_mat[, -4] <- matrix(unlist(lapply(tesseract_vec, cur_token_features, tesseract_vec, truth_set)), byrow = TRUE, ncol = 3)
-#   
-#   n <- 5
-#   for (i in 1:len){
-#     # cur_token <- tesseract_vec[i]
-#     # 
-#     # #f1 Presence of non alpha-numeric text within a word
-#     # f1 <- 0
-#     # l <- nchar(cur_token)
-#     # if ( l > 3){
-#     #   bulk <- substr(cur_token, start = 1, stop = l-1)
-#     #   s <- str_count(bulk, pattern = "[^[:alnum:]\\\"\\']")
-#     #   f1 <- s/l
-#     # }
-#     # 
-#     # #f2 1-gram frequency of a word
-#     # f2 <- sum(tesseract_vec == cur_token)/2
-#     # 
-#     # 
-#     # 
-#     # #f3
-#     # if(include_dist){
-#     #   all.words <- unique(c(english.words, truth_set))
-#     #   nv <- levenshtein.distance(tolower(cur_token), all.words)
-#     #   min_dist <- min(nv)
-#     #   if(min_dist > 2) { f3 <- l }
-#     #   else {
-#     #     candidate.words <- all.words[which.min(nv)]
-#     #     p <- min(sapply(candidate.words, num_pattern, cur_token))
-#     #     f3 <- (min_dist+(p/2)+1)/l
-#     #   }
-#     # }
-#     
-#     #f4 sliding window, 5-gram freq
-#     f4 <- 0
-# 
-#     
-#     # slide_freq <- rep(list(0), n)
-#     slide_window <- rep(list(NULL), n)
-#     
-#     if(i >= 5){slide_window[[1]] <- paste(low_tesseract_vec[(i-4):i], collapse = " ")}
-#     if(i >= 4 & i <= len - 1){slide_window[[2]] <- paste(low_tesseract_vec[(i-3):(i+1)], collapse = " ")}
-#     if(i >= 3 & i <= len - 2){slide_window[[3]] <- paste(low_tesseract_vec[(i-2):(i+2)], collapse = " ")}
-#     if(i >= 2 & i <= len - 3){slide_window[[4]] <- paste(low_tesseract_vec[(i-1):(i+3)], collapse = " ")}
-#     if(i <= len - 4){slide_window[[5]] <- paste(low_tesseract_vec[i:(i+4)], collapse = " ")}
-#     
-#     slide_freq <- unlist(lapply(slide_window, count_slide, gram_vec))
-#     
-#     f4 <- max(slide_freq, na.rm = TRUE)
-#     
-#     feature_mat[i,4] <- f4
-#   }
-#   return(feature_mat)
-# }
-  
-#   #feature 1
-#   l <- nchar(cur_token)
-#   feature_list[[1]] <- l
-#   
-#   #feature 2
-#   vowels <- '[aeiouAEIOU]'
-#   consonants <- '[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]'
-#   v <- str_count(cur_token, pattern = vowels)
-#   c <- str_count(cur_token, pattern = consonants)
-#   feature_list[[2]] <- c(v,c,v/l,c/l,ifelse(c>0,v/c,100))
-#   
-#   #feature 3
-#   s <- str_count(cur_token, pattern = '[^[:alnum:]]')
-#   feature_list[[3]] <- c(s, s/l)
-#   
-#   #feature 4
-#   d <- str_count(cur_token, pattern = '[0-9]')
-#   feature_list[[4]] <- c(d, d/l)
-#   
-#   #feature 5
-#   low <- str_count(cur_token, pattern = '[:lower:]')
-#   upp <- str_count(cur_token, pattern = '[:upper:]')
-#   feature_list[[5]] <- c(low, upp, low/l, upp/l)
-# 
-#   #feature 6
-#   string_split <- strsplit(cur_token, "")[[1]]
-#   consecutive_cnt <- rle(string_split)
-#   max_seq <- max(consecutive_cnt[[1]])
-#   feature_list[[6]] <- ifelse(max_seq>=3, max_seq/l, 0)
-# 
-#   #feature 7
-#   la <- str_count(cur_token, pattern = '[:alnum:]')
-#   feature_list[[7]] <- ifelse(s>la, 1, 0)
-# 
-#   #feature 8
-#   conso_token <- gsub(consonants, "b", cur_token)
-#   consecutive_conso <- rle(strsplit(conso_token, "")[[1]])
-#   max_conso_seq <- max(c(0, consecutive_conso[[1]][consecutive_conso[[2]] == "b"]))
-#   feature_list[[8]] <- ifelse(max_conso_seq>=6, 1, 0)
-# 
-#   #feature 9 
-#   token_infix <- substr(cur_token, start = 2, stop = l-1)
-#   n <- str_count(token_infix, pattern = '[^[:alnum:]]')
-#   feature_list[[9]] <- ifelse(n>=2, 1, 0)
-# 
-#   #feature 10 bigram
-#   cur_bigrams <- bigram_from_token(tolower(cur_token))
-#   n <- length(cur_bigrams)
-#   if(n == 1) { if(nchar(cur_bigrams) == 1) n <- 0.5 }
-#   feature_list[[10]] <- (sum(freq_bigrams[cur_bigrams],na.rm = TRUE)/10000)/n
-# 
-#   #feature 11
-#   max_freq <- max(table(string_split))
-#   feature_list[[11]] <- ifelse(max_freq>=3, max_freq/l, 0)
-# 
-#   #feature 12
-#   l1 <- str_count(cur_token, pattern = '[:alpha:]')
-#   l2 <- l - l1
-#   feature_list[[12]] <- ifelse(l1>0, l2/l1, 100)
-#   
-#   #feature 13 leveshtein distance
-#   if(include_dist){
-#     all.words <- unique(c(english.words, truth_set))
-#     nv <- levenshtein.distance(tolower(cur_token), all.words)
-#     min_dist <- min(nv)
-#     if(min_dist > 2) { f13 <- l }
-#     else {
-#       candidate.words <- all.words[which.min(nv)]
-#       p <- min(sapply(candidate.words, num_pattern, cur_token))
-#       f13 <- (min_dist+(p/2)+1)/l
-#     }
-#     feature_list[[13]] <- f13
-#   }
-#   ft <- unlist(feature_list)
-#   return(ft)
-# }
-
 token_to_input_format <- function(vec, freq_bigrams, truth_set){
   input_format <- matrix(unlist(lapply(vec,extract_feature,freq_bigrams,truth_set)), byrow = TRUE, nrow = length(vec))
   return(input_format)
